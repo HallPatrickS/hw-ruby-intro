81ca6a3f5cbb71bef691d1546bedb809e7768ba5
Patrick Hall <pshall95@gmail.com>
2018-09-10 23:04:00 -0700

    got max 2 sums

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -8,10 +8,30 @@ def sum arr
 end
 
 def max_2_sum arr
-  max = arr.max
-  arr.delete(max)
-  return max + arr.max
-  # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  end
+
+  if arr[0] > arr[1]
+    max = arr[0]
+    second_max = arr[1]
+  else
+    second_max= arr[0]
+    max = arr[1]
+  end
+
+  for i in arr do
+    next if (i == 0 or i == 1)
+    if i >= max
+      second_max = max
+      max = i
+    elsif i >= second_max
+      second_max = i
+    end
+  end
+  return max + second_max
 end
 
 def sum_to_n? arr, n
@@ -35,5 +55,5 @@ end
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+# # YOUR CODE HERE
 end
