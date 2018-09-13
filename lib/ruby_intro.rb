1d5ed686ae3d13a4541412c7d12537721396d765
jamesponwith <jponwith@sandiego.edu>
2018-09-12 23:19:57 -0700

    Getters, setters, and constructors good

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -55,5 +55,15 @@ end
 # Part 3
 
 class BookInStock
-  # # YOUR CODE HERE
-end
+	attr_accessor :isbn, :price
+	
+	def initialize(isbn, price) 
+		raise ArgumentError if isbn.empty? || price <= 0
+		@isbn = isbn
+		@price = price
+	end
+
+	def price_as_string(price)
+		
+	end	
+end		
